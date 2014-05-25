15d391ba9968d4d9b8e9ec7528c3573b33922f2b
Felipe <falmeida1988@gmail.com>
2014-05-24 21:17:55 -0300

    Agora tá certinho...

--- case1.py
+++ case1.py
@@ -8,19 +8,62 @@ from config.constants import *
 
 
 # does not distinguish between emails where an attribute appears more than
-# once as opposed to those wher the attribute appears only once.
+# once as opposed to those where said attribute appears only once.
+
+accuracy_in_each_turn = list()
 
 m = numpy.loadtxt(open("original_data/spambase.data","rb"),delimiter=',')
 
 for i in xrange(NUMBER_OF_ROUNDS):
-	shuffled = numpy.random.permutation(m)
+    shuffled = numpy.random.permutation(m)
 
-	train_set,test_set = prep.split_sets(shuffled,TRAIN_TEST_RATIO)
+    train_set,test_set = prep.split_sets(shuffled,TRAIN_TEST_RATIO)
 
-	p_char_spam = nb.take_p_char_spam(train_set,CASE_1_ATTRIBUTE_INDEX,SPAM_ATTR_INDEX)
-	p_spam = nb.take_p_spam(train_set,SPAM_ATTR_INDEX)
-	p_char = nb.take_p_attribute(train_set,CASE_1_ATTRIBUTE_INDEX,SPAM_ATTR_INDEX)
+    p_char_spam = nb.take_p_char_spam(train_set,CASE_1_ATTRIBUTE_INDEX,SPAM_ATTR_INDEX)
+    p_spam = nb.take_p_spam(train_set,SPAM_ATTR_INDEX)
+    p_char = nb.take_p_attribute(train_set,CASE_1_ATTRIBUTE_INDEX,SPAM_ATTR_INDEX)
 
-	p_spam_char = (p_char_spam * p_spam) / p_char
+    # whichever is greater - that will be our prediction
+    p_spam_char = (p_char_spam * p_spam) / p_char
+    p_ham_char = 1 - p_spam_char
 
-	print p_char_spam
+    if p_spam_char > p_ham_char:
+        guess = 1
+    else:
+        guess = 0
+
+    hits = 0
+    misses = 0
+
+    # now we test the hypothesis against the test set
+    for row in test_set:
+        if(row[CASE_1_ATTRIBUTE_INDEX] == 0):
+            if(row[SPAM_ATTR_INDEX] != guess):
+                hits += 1
+            else:
+                misses += 1
+        elif(row[CASE_1_ATTRIBUTE_INDEX] != 0):
+            if(row[SPAM_ATTR_INDEX] == guess):
+                hits += 1
+            else:
+                misses += 1
+
+    accuracy = float(hits)/float(hits+misses)
+
+    accuracy_in_each_turn.append(accuracy)
+
+mean_accuracy = numpy.mean(accuracy_in_each_turn)
+std_dev_accuracy = numpy.std(accuracy_in_each_turn)
+variance_accuracy = numpy.var(accuracy_in_each_turn)
+
+print ''
+print 'CASE 1 - ONE ATTRIBUTE ONLY'
+print 'MEAN_ACCURACY: '+str(mean_accuracy)
+print 'POP. STD. DEV. OF ACCURACY: '+str(std_dev_accuracy)
+print 'POP. VARIANCE OF ACCURACY: '+str(variance_accuracy)
+print ''
+
+
+
+
+    
