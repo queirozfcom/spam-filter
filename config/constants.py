57b46f6c3e43107af0937a0ad0927886db4c71d5
Clauvin <clauvin_almeida@hotmail.com>
2014-05-26 21:32:32 -0300

    Case 2.py
    Case 2, funcionando com 10 atributos.

--- config/constants.py
+++ config/constants.py
@@ -4,6 +4,21 @@ NUMBER_OF_ROUNDS = 10
 
 # 0-based
 CASE_1_ATTRIBUTE_INDEX = 51
+
+# chosen analyzed words:
+#   order       - 8
+#   receive     - 10
+#   addresses   - 14
+#   business    - 16
+#   email       - 17
+#   credit      - 19
+#   money       - 23
+#   hp          - 24
+#   george      - 26
+#   lab         - 28
+CASE_2_ATTRIBUTE_INDEX = [8,10,14,16,17,19,23,24,26,28]
+
+
 # 0-based
 SPAM_ATTR_INDEX = 57
 
